7e2962fd4af25b7ec871226233031636f445e629
terwer <youweics@163.com>
2022-12-27 17:43:38 +0800

    fix: 设置命名规则

--- src/picgo.ts
+++ src/picgo.ts
@@ -4,7 +4,18 @@ let picgo
 
 async function activate(configPath?: string) {
   picgo = new PicGo(configPath)
-  console.log("picgo core v1.5.0 activated.")
+
+  picgo.setConfig({
+    // 设置图床插件
+    // uploader: 'smms',
+    // smms: {
+    //   token: 'your-api-token',
+    // },
+    // 设置重命名规则
+    rename: '${random}-${year}-${month}-${day}-${hour}-${minute}-${second}',
+  });
+
+  console.log("picgo core v1.5.0 activated.custom configPath", configPath)
 }
 
 async function upload(input?: any[]) {
@@ -35,10 +46,16 @@ function deactivate() {
   console.log("picgo deactivated.")
 }
 
+function getPicgoObj(){
+  console.log("get current picgo object.")
+  return picgo
+}
+
 const picgoExtension = {
   activate,
   deactivate,
   upload,
   uploadFormClipboard,
+  getPicgoObj
 }
 export default picgoExtension
