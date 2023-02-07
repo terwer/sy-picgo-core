5f4c6e6096b4e77fc0591b7402b0245b9a9520e3
terwer <youweics@163.com>
2023-02-07 11:54:19 +0800

    fix: 优化PicGO调用

--- src/picgo.ts
+++ src/picgo.ts
@@ -1,65 +1,105 @@
 import { PicGo } from "picgo"
+import pkg from "../package.json"
 
-let picgo
+/*
+ * 思源笔记内部PicGO对象定义
+ */
+class SyPicgo {
+  private picgo
 
-async function activate(configPath?: string) {
-  picgo = new PicGo(configPath)
-  console.log("picgo core v1.5.0 activated.custom configPath", configPath)
-}
+  constructor(configPath: string) {
+    this.picgo = new PicGo(configPath)
+    console.log("picgo core inited.configPath", configPath)
+  }
 
-async function upload(input?: any[]) {
-  let ret
-  console.log("PicGo is uploading...")
-  try {
-    const result = await picgo.upload(input)
-    if (result instanceof Array) {
-      ret = result
-      console.log("upload success.total=>" + result.length)
+  /**
+   * 初始化
+   */
+  activate() {
+    this.picgo.saveConfig({
+      debug: true,
+      PICGO_ENV: "SY-PICGO",
+    })
+    this.picgo.GUI_VERSION = pkg.version
+    console.log("picgo core v1.5.0 activated.")
+  }
+
+  /**
+   * 上传图片
+   * @param input 图片数组
+   */
+  public async upload(input?: any[]) {
+    let ret
+    console.log("PicGo is uploading...")
+    try {
+      const result = await this.picgo.upload(input)
+      if (result instanceof Array) {
+        ret = result
+        console.log("upload success.total=>" + result.length)
+      }
+    } catch (e) {
+      console.error("upload error", e)
+      throw new Error(
+        "upload error, please check you picgo config!Detail info:" +
+          JSON.stringify(e)
+      )
     }
-  } catch (e) {
-    console.error("upload error", e)
-    throw new Error(
-      "upload error, please check you picgo config!Detail info:" +
-        JSON.stringify(e)
-    )
-  }
 
-  console.log("ret=>", ret)
-  return JSON.stringify(ret)
-}
-
-async function uploadFormClipboard() {
-  let ret
-  console.log("PicGo is uploading form clipboard... ")
-  try {
-    ret = await upload()
-    console.log("upload success.")
     console.log("ret=>", ret)
-  } catch (e) {
-    console.error("upload error", e)
-    throw new Error(
-      "upload error, please check you picgo config!Detail info:" +
-        JSON.stringify(e)
-    )
+    return JSON.stringify(ret)
   }
-  return ret
-}
 
-function deactivate() {
-  picgo = null
-  console.log("picgo deactivated.")
-}
+  /**
+   * 从剪贴板上传图片
+   */
+  public async uploadFormClipboard() {
+    let ret
+    console.log("PicGo is uploading form clipboard... ")
+    try {
+      ret = await this.picgo.upload()
+      console.log("upload success.")
+      console.log("ret=>", ret)
+    } catch (e) {
+      console.error("upload error", e)
+      throw new Error(
+        "upload error, please check you picgo config!Detail info:" +
+          JSON.stringify(e)
+      )
+    }
+    return ret
+  }
 
-function getPicgoObj() {
-  console.log("get current picgo object.")
-  return picgo
+  /**
+   * 销毁PicGO对象
+   */
+  deactivate() {
+    this.picgo = null
+    console.log("picgo deactivated.")
+  }
+
+  /**
+   * 获取PicGO对象
+   */
+  public getPicgoObj() {
+    console.log("get current picgo object.")
+    return this.picgo
+  }
 }
 
 const picgoExtension = {
-  activate,
-  deactivate,
-  upload,
-  uploadFormClipboard,
-  getPicgoObj,
+  initPicgo: (configPath) => {
+    const syPicgo = new SyPicgo(configPath)
+    syPicgo.activate()
+    window.SyPicgo = syPicgo
+    console.log("挂载window.SyPicgo", window.SyPicgo)
+  },
+  destroyPicgo: () => {
+    if (!window.SyPicgo) {
+      return
+    }
+    window.SyPicgo.deactivate()
+    console.log("销毁window.SyPicgo", window.SyPicgo)
+  },
 }
+
 export default picgoExtension
