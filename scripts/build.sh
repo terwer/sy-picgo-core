c465f85bc9d08c56b43bbd139adb113c438ebf51
terwer <youweics@163.com>
2023-02-14 17:37:51 +0800

    fix: 修复构建脚本

--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-node esbuild.mjs --production
-echo "build success."
-cp ./dist/syPicgo.js ../src-sy-post-publisher/public/lib/picgo/syPicgo.js
-cp ./dist/syPicgo.js ../my-note-docker/workspace/SiYuan/data/widgets/sy-post-publisher/lib/picgo/syPicgo.js
-echo "copy success."
